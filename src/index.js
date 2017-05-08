be8c6a3ac47458a07a1df82d6b6d8d97caa95553
markdegroat <mhdegroat@gmail.com>
2017-05-08 10:35:24 -0400

    Added profanity filter so that people cannot write profanity as comments

--- src/index.js
+++ src/index.js
@@ -20,17 +20,44 @@ function CommentList(array_of_comments){
   }
 }
 
+function ContainsSwearWord(string){
+  var swear_bool = false
+
+  if(string.toLowerCase().indexOf("fuck") >= 0){
+    swear_bool = "true"
+  }
+  if(string.toLowerCase().indexOf("shit") >= 0){
+    swear_bool = "true"
+  }
+  if(string.toLowerCase().indexOf("ass") >= 0){
+    swear_bool = "true"
+  }
+  if(string.toLowerCase().indexOf("butt") >= 0){
+    swear_bool = "true"
+  }
+  if(string.toLowerCase().indexOf("stupid") >= 0){
+    swear_bool = "true"
+  }
+  if(string.toLowerCase().indexOf("mark") >= 0){
+    swear_bool = "true"
+  }
+  return swear_bool
+}
+
 $(document).ready(function(){
   var comment_list = new CommentList([])
 
   $("#note-form").submit(function(e){
     e.preventDefault
     //preventing user from adding empty comments
-    if($("input:text").val() != ''){
+    if($("input:text").val() != '' && !ContainsSwearWord($("input:text").val())){
       var comment = new Comment($("input:text").val())
       comment_list.addComment(comment)
       $("input:text").val('')
       $("#comment-list").html(comment_list.render())
+    }else{
+      alert("You're either not entering anything or being a potty mouth, either way knock it off!")
+      $("input:text").val('')
     }
     return false
   })
